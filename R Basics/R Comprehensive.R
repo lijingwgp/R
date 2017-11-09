eb3c6f4e582ef9b9a33187e6fccc57543dd4be59
Jing <lijingwgp@yahoo.com>
2017-11-08 20:15:00 -0500

    Update R Comprehensive.R

--- R Basics/R Comprehensive.R
+++ R Basics/R Comprehensive.R
@@ -152,30 +152,6 @@ mytable3 <- table(actuals[,1], predicted[,3])
 # Model 3 error rate
 (mytable3["Up", "Down"] + mytable3["Down", "Up"]) / sum(mytable3)
 # Which model is better (trick question!)
-###########################################################################################
-## Partitioning a dataframe into train, validate, test sets of specified relative sizes ###
-###########################################################################################
-set.seed(5072)             #sets the random seed
-nobs <- nrow(SandP) 
-trainprop <- 0.7           #say we want a 70/20/10 split of rows for training, validation and test respectively
-validateprop <- 0.2
-#create a vector of random integers of training size from the vector 1:nobs
-train  <-  sample(nobs, trainprop * nobs)
-#create a vector of the remaining  integers, then create a vector of random integers
-#of validate size by sampling from these
-validate  <-  sample(setdiff(1:nobs, train), validateprop * nobs) 
-#create a vector of the integers not in either training or validate
-test <- setdiff(setdiff(1:nobs, train), validate)
-#Create the data frames using the indices created in the three vectors above
-trainset <- SandP[train,]
-validateset <- SandP[validate,]
-testset <- SandP[test,]
-#Checks
-nobs
-nrow(trainset) / nobs
-nrow(validateset) / nobs
-nrow(testset) / nobs
-set.seed(NULL)             #remove the random seed 
 ########################################################################
 #### Reading Data from Excel, SQL Server and JSON File Structures ######
 ########################################################################
