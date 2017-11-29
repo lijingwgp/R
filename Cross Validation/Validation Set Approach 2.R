8028f22e50529d52c47ad4e870c4d920c389018b
Jing <lijingwgp@yahoo.com>
2017-11-29 18:00:36 -0500

    Update Validation Set Approach 2.R

--- Cross Validation/Validation Set Approach 2.R
+++ Cross Validation/Validation Set Approach 2.R
@@ -1,8 +1,15 @@
 rm(list=ls())
 # Here's a way we haven't used before:
-Hitters=na.omit(Hitters)
+Hitters = na.omit(Hitters)
 set.seed(1)
-train=sample(c(TRUE,FALSE), nrow(Hitters),rep=TRUE)
-test=(!train)   #test=(-train)
-Hitters.train = Hitters[train,]
-Hitters.test = Hitters[test,]
+
+train = sample(c(TRUE,FALSE), nrow(Hitters),rep=TRUE)
+train = sample(nrow(Hitters)/2,nrow(Hitters)*0.8)
+test = (!train)
+test = (-train)
+
+train = sample(nrow(Hitters),nrow(Hitters)*0.8)
+test = setdiff(1:nrow(Hitters), train)
+hitter.train = Hitters[train,]
+hitter.test = Hitters[test,]
+salary_test = Hitters$Salary[test]
