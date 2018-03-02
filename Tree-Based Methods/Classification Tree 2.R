1588012f212ac32042b4bddb3ae11ae8f9eb6280
Jing <lijingwgp@yahoo.com>
2018-03-02 13:32:52 -0500

    Update Classification Tree 2.R

--- Tree-Based Methods/Classification Tree 2.R
+++ Tree-Based Methods/Classification Tree 2.R
@@ -41,7 +41,7 @@ printcp(rpart)
 plotcp(rpart)
 
 
-###access test error rates###
+###access training error rates###
 preds <- predict(rpart, newdata = test, type="class")
 table.unpruned <- table(test$churn, preds, dnn=c("Actual", "Predicted"))
 table.unpruned
@@ -58,7 +58,7 @@ rpart.prune$cptable
 fancyRpartPlot(rpart.prune, main="Pruned Customer Churn Prediction Model")
 
 
-###evaluate predictive power using test dataset###
+###access test error rate###
 predict <- predict(rpart.prune, newdata=test, type="class")
 table1 <- table(test$churn, predict, dnn=c("Actual", "Predicted"))
 table1
@@ -80,6 +80,8 @@ table.gini
 
 
 ###add loss matrix###
+###6 is added to the type II error spot which indicates we would like to gether more data###
+###when we predict no but it is actually yes###
 rpart2 <- rpart(churn ~ ., data=train, method="class", parms=list(split="information"), control=rpart.control(usesurrogate=0, 
           maxsurrogate=0, cp=0, minsplit=2, minbucket=1, loss = matrix(c(0,1,6,0), byrow = T, nrow = 2)))
 rpart2$cptable
@@ -93,11 +95,19 @@ predict2 <- predict(rpart2.prune, newdata=test, type="class")
 table2 <- table(test$churn, predict2, dnn=c("Actual", "Predicted"))
 table2
 
+###type = prob will yield probabilities###
+###now predict3 would become a matrix with two columns, no and yes, that each###
+###is predicting the probabilities of a test observation is belong to no or yes###
+###then we will compare all the probabilities of no's to the response column of the test data set###
+###we use the no column primarily bacause we are interested in type II error###
+###if we are interested in the type I error, then we would use the yes column###
 predict3 <- predict(rpart.prune, newdata=test, type="prob")
 roc1 <- roc(test$churn, predict3[,1])
 plot(roc1, print.auc=TRUE, auc.polygon=TRUE, grid=c(0.1, 0.2), 
      grid.col=c("green", "red"), max.auc.polygon=TRUE,
      print.thres = TRUE, xaxs = "i", yaxs = "i", main = 'Churn ROC Curve')
+###we construct a yes column then change the corresponding element to no after we adjust the probability###
+###this is because we want to reduce the type II error###
 predict4 <- rep('Yes', nrow(predict3))
 predict4[predict3[,1] >= .860] <- 'No'
 table3 <- table(test$churn, predict4, dnn = c("Actual", "Predicted"))
