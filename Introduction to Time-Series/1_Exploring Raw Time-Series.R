3cb747cb606310e3305e258183d0ab4deddc598a
Jing <lijingwgp@yahoo.com>
2018-09-08 12:54:30 -0400

    Update 1_Exploring Raw Time-Series.R

--- Introduction to Time-Series/1_Exploring Raw Time-Series.R
+++ Introduction to Time-Series/1_Exploring Raw Time-Series.R
@@ -64,8 +64,10 @@ plot(Nile, xlab = "Year", ylab = "River Volume (1e9 m^{3})",
 start(AirPassengers)
 end(AirPassengers)
 time(AirPassengers)
-deltat(AirPassengers)
 frequency(AirPassengers)
+
+deltat(AirPassengers)
+time(AirPassengers)
 cycle(AirPassengers)
 
 # Sometimes it is useful to know the missing values locations, since we may want to impute 
